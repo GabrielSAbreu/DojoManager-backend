e2da924316fb3ff3736d410967cc1e149c3cbd77
Gabriel da Silva Abreu <gabrielsabreu.dev@gmail.com>
2026-06-09 23:32:31 -0300

    chore: Adicionado middleware para realizar testes entre api e front no ambiente de desenvolviment/local

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI, HTTPException, status
 from typing import List
 from model import Session
-
-# from model.modalidade import Modalidade
-# from schema import ModalidadeCreate, ModalidadeResponse, UsuarioCreate, UsuarioResponse
+from fastapi.middleware.cors import CORSMiddleware
 from routes import modalidade_router, usuario_router, pratica_router
 
 app = FastAPI()
+
+origins = ["*"]  # Permitir todas as origens afim de facilitar o teste local
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(modalidade_router)
 app.include_router(usuario_router)
